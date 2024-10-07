c76bd0d03948e562d8127bb1722b1fab1c067d72
Taj Jethwani-Keyser <tjethwanikeyser@college.harvard.edu>
2024-10-07 12:29:42 -0400

    eslint fixes and ts downgrade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: "Offline AI",
