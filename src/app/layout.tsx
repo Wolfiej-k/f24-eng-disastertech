168b6977b347f168048f329ec3723b3a9d48f0fd
Wolfie <wolfiejetkey@gmail.com>
2025-01-04 02:53:41 -0500

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default async function RootLayout({
   const user = await getUser();
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body className="h-screen overflow-hidden font-sans">
         <Navbar user={user} />
         <main className="h-full overflow-auto pt-24">{children}</main>
